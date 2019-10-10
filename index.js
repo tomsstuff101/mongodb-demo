dbc38bced7b0601833255bd78c49b080a861e05a
CN105 <cn105@CN0105.local>
2019-10-10 14:47:13 +0100

    create schema and added hardcoded data

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const User = require('./models/user')
 
 mongoose.connect('mongodb://localhost:27017/signup',{useNewUrlParser:true})
 
+
+
+// filling in data into the schema, which is an object, 
+// give it a string
+
+const user = new User({
+    name:'your name',
+    email:'yourName@mail.com',
+    password:'secretpassword'
+})
+
+
+user.save()
--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+
+// create a schema, this is akin to CREATE TABLE in sql
 const user = new mongoose.Schema({
 name:{type:String, required:true},
 email:{type:String, required:true},
